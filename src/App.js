794433336955c70dd555c73a79e7ef4216b7f522
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2023-03-22 10:47:05 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,38 +3,39 @@ import './App.css';
 import Dice from "./images/icon-dice.svg"
 import divider from './images/pattern-divider-desktop.svg';
 import mobileDivider from "./images/pattern-divider-mobile.svg"
+import facebook from "./images/square-facebook.svg"
+import twitter from "./images/square-twitter.svg"
 const url="	https://api.adviceslip.com/advice";
 function App() {
+  const [errorShow,setErrorShow]=useState(false);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [advice, setAdvices] = useState([]);
   useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setAdvices(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
+    fetchData();
   }, []);
   const fetchData=()=>{
+    setIsLoaded(false);
     fetch(url)
-      .then(res => res.json())
+      .then((res) =>{
+      if(!res.ok)
+      {
+        throw new Error('Network response was not ok');
+      }
+      return res.json();
+      })
       .then(
         (result) => {
+          setErrorShow(false);
           setIsLoaded(true);
           setAdvices(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
         }
-      )
+      ).catch((error) => {
+        console.log(error);
+        setErrorShow(true);
+        setIsLoaded(false);
+        setError(error);
+      });
   }
   let text="";
   let id="";
@@ -42,64 +43,41 @@ function App() {
       id+=`${advice[x].id}`;
       text+=`${advice[x].advice}`;
   }
-  if (error) {
-    return ( 
-    <div className="App">
-      <div className="header">Error</div>
-      <div className="message">
-        <div className='text'>
-          Error:{error.message}
-        </div>
-      </div>
-      <div className="divider">
-        <img src={divider} alt="divider"/>
-      </div>
-      <div className="footer">
-        <div className='box'>
-        <img src={Dice} className="dice" alt="Dice"/>
-      </div>
-      </div>
-    </div>
-    );
-  } else if (!isLoaded) {
+  function shareOnFacebook() {
+    var shareUrl = "https://www.facebook.com/dialog/share?app_id=100074538132167&display=popup&quote=" + encodeURIComponent(text);
+    window.open(shareUrl, "_blank");
+  }
+  function shareOnTwitter() {
+    window.open(`https://twitter.com/intent/tweet?text=${text}&hashtags=advice`);
+  }
     return (
-    <div className="App">
-      <div className="header">Loading</div>
-      <div className="message">
-        <div className='text'>
-          Loading....
+    <div className="App" id="quote-box">
+      <div className="header" id="author">
+      {errorShow && !isLoaded && <span>Error</span>}
+      {!errorShow && isLoaded && <span>Advice #{id}</span>}
+      {!errorShow && !isLoaded && <span>Loading</span>}
         </div>
-      </div>
-      <div className="divider">
-        <img src={divider} alt="divider"/>
-      </div>
-      <div className="footer">
-        <div className='box'>
-        <img src={Dice} className="dice" alt="Dice"/>
-      </div>
-      </div>
-    </div>
-    );
-  } else {
-    return (
-    <div className="App">
-      <div className="header">Advice #{id}</div>
       <div className="message">
-        <div className='text'>
-          <span>{text}</span>
+        <div className='text' id="text">
+          {!errorShow && isLoaded && <span>{text}</span>}
+          {!errorShow && !isLoaded && <span>Loading...</span>}
+          {errorShow && !isLoaded && <span>Error:{error.message}</span>}
         </div>
       </div>
       <div className="divider">
         <img src={divider} alt="divider" className='desktopDivider'/>
         <img src={mobileDivider} alt="divider" className='mobileDivider'/>
       </div>
+      <div className="socialMedia">
+        <a href className="fb"><img src={facebook} width="50" onClick={shareOnFacebook} height="50" alt="facebook"/></a>
+        <a href id="tweet-quote" className="fb"><img src={twitter} width="50" onClick={shareOnTwitter} height="50" alt="twitter"/></a>
+      </div>
       <div className="footer">
-        <div className='box'>
-        <img src={Dice} className="dice" onClick={fetchData} alt="Dice"/>
+        <div className='box' onClick={fetchData} id="new-quote">
+        <img src={Dice} className="dice" alt="Dice"/>
       </div>
       </div>
     </div>
   );
-  }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-console.log(document.querySelector(".text"));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
